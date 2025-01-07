e4b6dabf7a0d2d55a6bb363ee10e5e1bdd1d75ee
AkarshanGhosh <ronighosh494@gmail.com>
2025-01-07 18:22:51 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -10,10 +10,11 @@ import userRouter from "./routes/userRoutes.js";
 const app = express();
 const port = process.env.PORT || 4000
 connectDB();
+const allowedOrigins =['http://localhost:5173']; // Frontend URL
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true}));
+app.use(cors({origin: allowedOrigins, credentials: true}));
 
 
 //API Endpoints
