d7a18bddab73b7d4752334110b49e41ae745ab77
NikhilKumarNayak19 <125073240+NikhilKumarNayak19@users.noreply.github.com>
2025-01-10 16:54:28 +0530

    front

---
+++ Client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div  style={{
+        backgroundColor: "#27272a", // Zinc 800 in Tailwind is #27272a
+        color: "white",
+        padding: "8px 16px", // py-2 (8px top/bottom) and px-4 (16px left/right)
+      }}>Navbar</div>
+  )
+}
+
+export default Navbar
