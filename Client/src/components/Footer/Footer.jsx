1d7221b4262ffbfb6d755b7fae06b04058a37060
AkarshanGhosh <ronighosh494@gmail.com>
2025-01-11 13:48:41 +0530

    updating

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+//import React from 'react'
 
 const Footer = () => {
   return (
     
     <div className='bg-zinc-700 text-white px-8 py-4 rounded'>
       <h1 className='text-2xl font-semibold text-center'>
-        &copy; 2025, Made by ECE_HackBot
+        &copy; 2025, Legal Read all rights reserved
       </h1>
     </div>
   )
