e3c654669131fcad894dfd66302a30b40e76221f
AkarshanGhosh <ronighosh494@gmail.com>
2025-01-13 00:23:48 +0530

    admin dashboard

---
+++ Client/src/components/Admin/AdminDashboardBody.jsx
@@ -0,0 +1,11 @@
+//import React from 'react'
+
+const AdminDashboardBody = () => {
+  return (
+    <div>
+      hello admin
+    </div>
+  )
+}
+
+export default AdminDashboardBody
---
+++ Client/src/pages/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+//import React from 'react'
+
+import AdminDashboardBody from "../components/Admin/AdminDashboardBody"
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <AdminDashboardBody/>
+    </div>
+  )
+}
+
+export default AdminDashboard
