ef0043f369707f7f653c6fd96404ce84e552a585
AkarshanGhosh <ronighosh494@gmail.com>
2025-01-12 13:33:21 +0530

    debugging

--- Client/src/components/ResourceCard/ResourceCard.jsx
+++ Client/src/components/ResourceCard/ResourceCard.jsx
@@ -10,13 +10,14 @@ const ResourceCard = ({ data }) => {
       <div className="bg-zinc-800 rounded p-4 flex flex-col">
         <div className="bg-zinc-900 rounded flex items-center justify-center">
           <img
-            src={data.url}
+            src={data.image}
             alt={data.title || "Resource"}
             className="w-full h-[25vh] object-cover rounded"
           />
         </div>
         <h2 className="mt-4 text-xl  font-semibold">{data.title}</h2>
         <p className="mt-2 text-zinc-400 font-semibold">by {data.author}</p>
+        <p className="mt-2 text-zinc-400 font-semibold">Category: {data.category}</p>
       </div>
     </Link>
   );
@@ -28,6 +29,8 @@ ResourceCard.propTypes = {
     url: PropTypes.string.isRequired,
     title: PropTypes.string,
     author: PropTypes.string,
+    image: PropTypes.string,
+    category:PropTypes.string,
   }).isRequired,
 };
 
