74142c8414cc31f60650dac9a53e96c4bb77f43d
AkarshanGhosh <ronighosh494@gmail.com>
2025-01-13 16:18:21 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import connectDB from "./config/mongodb.js";
-import authRouter from './routes/authRoutes.js';
+import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import resourceRouter from "./routes/resource.js";
 import readLaterRouter from "./routes/readLaterRoutes.js";
@@ -17,18 +17,18 @@ connectDB();
 
 // Define allowed origins for CORS
 const allowedOrigins = [
-    'http://localhost:5173',
-    'https://clinquant-cuchufli-da38a3.netlify.app' // Add deployed frontend URL
+  "http://localhost:5173",
+  "https://clinquant-cuchufli-da38a3.netlify.app", // Add deployed frontend URL
 ];
 
 // Configure CORS options
 app.use(
-cors({
+  cors({
     origin: "https://e-book-mern-app.vercel.app/",
-    methods: ['GET', 'POST','UPDATE','PUT'],
+    methods: ["GET", "POST", "UPDATE", "PUT"],
     Credentials: true,
-})
-)
+  })
+);
 
 // Middleware
 app.use(express.json());
@@ -36,11 +36,11 @@ app.use(cookieParser());
 app.use(cors(corsOptions));
 
 // API Endpoints
-app.get('/', (req, res) => res.send("API is running"));
-app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter);
-app.use('/api/resource', resourceRouter);
-app.use('/api/readlater', readLaterRouter);
+app.get("/", (req, res) => res.send("API is running"));
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/resource", resourceRouter);
+app.use("/api/readlater", readLaterRouter);
 
 // Start the server
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
