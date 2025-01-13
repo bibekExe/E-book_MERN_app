07b2268b61601ba42312e78796362f01816886ba
AkarshanGhosh <ronighosh494@gmail.com>
2025-01-13 16:11:40 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -23,14 +23,7 @@ const allowedOrigins = [
 
 // Configure CORS options
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    origin: "*",
 };
 
 // Middleware
